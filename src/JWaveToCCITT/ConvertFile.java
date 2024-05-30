6f325c8ef6bcb3b438f0932b444239dbd5999aa7
dcrandell <david@onholdwizard.com>
2024-05-30 04:17:00 -0700

    first commit

---
+++ src/JWaveToCCITT/ConvertFile.java
@@ -0,0 +1,55 @@
+package JWaveToCCITT;
+
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class ConvertFile {
+    String filePath;
+    String fileDest;
+    ConvertFile(String filePath, String fileDest) {
+        this.filePath = filePath;
+        this.fileDest = fileDest;
+    }
+
+    public ArrayList<String> getFileList() {
+        ArrayList<String> fileList = new ArrayList<>();
+        File dir = new File(this.filePath);
+        File[] files = dir.listFiles((dir1, name) -> name.contains("wav") || name.contains("mp3"));
+        if (files != null) {
+            for (File file : files) {
+                fileList.add(file.getName());
+            }
+        }
+        return fileList;
+    }
+
+    public boolean convertFiles() throws UnsupportedAudioFileException, IOException {
+        ArrayList<String> files = getFileList();
+        try {
+            for (String file : files) {
+                File inputFile = new File(this.filePath + file);
+                File outputFile = new File(this.fileDest + file);
+
+                AudioInputStream wavStream = AudioSystem.getAudioInputStream(inputFile);
+                AudioFormat wavFormat = wavStream.getFormat();
+                AudioFormat uLawFormat = new AudioFormat(
+                        AudioFormat.Encoding.ULAW,
+                        8000,
+                        8,
+                        1,
+                        1,
+                        wavFormat.getSampleRate(),
+                        false
+                );
+                AudioInputStream uLawStream = AudioSystem.getAudioInputStream(uLawFormat, wavStream);
+                AudioSystem.write(uLawStream, AudioFileFormat.Type.WAVE, outputFile);
+            }
+        } catch (IOException | UnsupportedAudioFileException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+        return true;
+    }
+}
---
+++ src/JWaveToCCITT/Main.java
@@ -0,0 +1,27 @@
+package JWaveToCCITT;
+
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws UnsupportedAudioFileException, IOException {
+        if (args.length != 2) {
+            System.err.println("Please provide a source and destination path");
+            System.exit(1);
+        }
+        try {
+            String filePath = args[0];
+            String fileDest = filePath + args[1];
+            ConvertFile convert = new ConvertFile(filePath, fileDest);
+            for (String f : convert.getFileList()) {
+                System.out.println(f);
+            }
+            boolean result = convert.convertFiles();
+            System.out.println("Result of process: " + String.valueOf(result));
+        }
+        catch (UnsupportedAudioFileException | IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
